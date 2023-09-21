06ebb4ee6776c8876eb34133981a5230409e559d
kiixen <moxinur.chan88@gmail.com>
2023-09-21 22:07:15 +0500

    done.issue

---
+++ issue1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+print(f"Your name: {name}")
+age = input ('age:')
+if int(age) >= 18:
+    print ("Warm greetings," + name,'.^^' )
+else: print ('No access' + ".")
