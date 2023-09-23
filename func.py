eb8f82b929d1f201c8836f73974bd60622764a9f
Sasha Raimov <cyberhub.python@gmail.com>
2023-09-23 18:49:01 +0500

    def

---
+++ func.py
@@ -0,0 +1,20 @@
+def printHello() :
+    print('hello world');
+
+
+# printHello()
+
+def calculate(x, y, operation):
+    if operation == '+':
+        return f"Result: {int(x) + int(y)}!";
+    elif operation == '-':
+        return f"Result: {int(x)-int(y)}!";
+    else:
+        return "wrong operation";
+
+def a():
+    return 'a';
+    4+5;
+
+
+print(calculate(input('enter first number'),input('enter second number'), input('enter operation')))
