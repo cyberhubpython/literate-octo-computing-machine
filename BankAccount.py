6d7130f5a44ad9b5658390c269ac9d9305587345
Sasha Raimov <cyberhub.python@gmail.com>
2023-10-18 12:21:40 +0500

    fix

--- BankAccount.py
+++ BankAccount.py
@@ -4,11 +4,39 @@ class Account:
         self.sirname = sirname
         self.fathername = fathername
         self.balance = balance
+    
     def infoAccount(self):
         print("This specific account's data is: " + self.name + self.sirname + self.fathername, + self.balance)
+    
     def addBalance(self, amount):
         self.balance += amount
+    
     def substractBalance(self, amount):
+        if self.balance < amount:
+            raise Exception
+        
         self.balance -= amount
 
+
+        return self
+
+
 personsAcc = Account("Nathan ", "Flame ", "de Satan:", 200000)
+count = 0;
+
+def pokupka():  
+    global personsAcc, count;
+    try:
+
+        personsAcc.substractBalance(100000);
+        count+=1;
+        print('спасибо за покупку айфона', count, personsAcc.balance);
+    except:
+        print('ошибка транзакции');
+
+
+pokupka();
+pokupka();
+pokupka();
+
+print('приходите еще');
--- GuessNumber.py
+++ GuessNumber.py
@@ -9,37 +9,39 @@ helpRequest = False
 guessNumber = 0
 
 while attempts > 0:
-    guessNumber = random.randint(1,10)
-    attempts -= 1
-    if level %5 == 0:
-        helps += 1
-        answers += 1
-    
-    user_input = input(f"Try to guess number. Alternatevely, ask for help - h, or ask for direct answer - a: ")
+        
+    try:
+        guessNumber = random.randint(1,10)
+        attempts -= 1
+        if level %5 == 0:
+            helps += 1
+            answers += 1
+        
+        user_input = input(f"Try to guess number. Alternatevely, ask for help - h, or ask for direct answer - a: ")
 
-    if user_input.lower() == "h" and helps > 0:
-        helpRequest = True
-        helps -= 1
-        print("The number is even" if guessNumber %2 == 0 else "The number is odd")
-        user_input = int(input("Try to guees number: "))
+        if user_input.lower() == "h" and helps > 0:
+            helpRequest = True
+            helps -= 1
+            print("The number is even" if guessNumber %2 == 0 else "The number is odd")
+            user_input = int(input("Try to guees number: "))
 
-        if user_input == guessNumber:
-            points += 2
-            print("You've guessed right")
+            if user_input == guessNumber:
+                points += 2
+                print("You've guessed right")
+            else:
+                print("You are wrong!")
+        
+        elif user_input.lower() == "a" and answers > 0:
+            print(f"The answer is {guessNumber}")
+            answers -= 1
+            points += 1
+        
+        elif (int(user_input)) == guessNumber:
+            print("You've guessd right!")
+            points += 3
         else:
             print("You are wrong!")
-    
-    elif user_input.lower() == "a" and answers > 0:
-        print(f"The answer is {guessNumber}")
-        answers -= 1
-        points += 1
-    
-    elif (int(user_input)) == guessNumber:
-        print("You've guessd right!")
-        points += 3
-    else:
-        print("You are wrong!")
-
-
+    except:
+        print('error');
 
 print(points)
