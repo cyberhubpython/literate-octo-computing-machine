a250b68dc36ea154a42c5739281b0f7cd2bb7163
Sasha Raimov <crazyysasha@gmail.com>
2023-10-19 20:12:55 +0500

    fix

--- sleep.py
+++ sleep.py
@@ -2,7 +2,25 @@ import time
 
 
 count = 0;
+# last = int(input('create seconds'));
+# last = 130;
+# while True:
+#     time.sleep(1)
+#     if count > last:
+#         break;
+#     count+=1;
+
+#     print(f'{ (last - count ) // 60 } : {(last - count) % 60}');
+
+# print('время вышло');
+
+
+seconds = 0;
+count = 0;
 while True:
-    time.sleep(1)
-    count+=1;
-    print(count);
+    time.sleep(0.001)
+    print(f'{(count // (60*60)) % (60*60)}:{(count // 60) % 60}:{count % 60}');
+    count+=1
+    seconds = count % 60;
+
+    print(seconds);
