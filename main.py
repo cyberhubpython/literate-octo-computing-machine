7f39dc90cda35ab20029b4b237c2a191f24fe4ad
Sasha Raimov <crazyysasha@gmail.com>
2023-10-31 18:56:34 +0500

    decorators Fixes #22

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+from time import time
 
-currentCountry = 'Uzbekistan'
-# my countries list
-countries = [ currentCountry, 'Russia'];
+def timer(a):
+    print(a);
+    def dec(fn):
+        print('decorated', fn.__name__);
+        def f(*arg, **kwargs):
+            startTime = time();
+            fn(*arg, **kwargs);
+            endTime = time();
+            print(endTime - startTime);
+        return f;
+    return dec;
 
-print(countries)
+@timer('osnova')
+def main (a):
+    for x in range(1000000):
+        print(x, a);
 
-print(countries.sort());
+@timer('osnova2')
+def main2(a,b):
+    print('asd', a, b);
+
+
+
+# main('as');
+# main2('asf', 'asg');
