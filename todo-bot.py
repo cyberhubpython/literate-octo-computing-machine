3df3b71848ed5964dd90ed121bf35d483565410f
Sasha Raimov <crazyysasha@gmail.com>
2023-11-02 20:15:30 +0500

    fix

--- todo-bot.py
+++ todo-bot.py
@@ -12,9 +12,15 @@ class Todo:
 
 
 
-accountsData = {
-    # message.chat.id : [Todo(title, priority)]
-}
+# def decor(fn, **args):
+#     print('hi from decorator', args);
+#     def d(*arg, **kwargs):
+#         print(kwargs['event_update']['message']);
+#         for x in kwargs.keys():
+#             print(x);
+#         return fn(*arg, **kwargs)
+    
+#     return d
 
 
 @dp.message(Command('start'))
@@ -24,22 +30,81 @@ async def command_start_handler(message: Message):
 welcome!
 my commands:
 /create - for create new task with format: {title}-{priority}
+/all -
+/done - 
+/clear -
 """);
-    accountsData[message.chat.id] = [];
 
-
+isCreateCalled = False;
 
 @dp.message(Command("create"))
-
 async def command_create_handler(message: Message):
-    title, priority = message.text.replace('/create', '').split('-')
-    accountsData[message.chat.id].append(Todo(title, priority))
+        context = preparation(message);
+        global isCreateCalled;
+        try:
+            if '-' not in message.text and len(message.text.split(' ')) < 2:
+                async def defineTitle(message):
+                    context['newTaskTitle'] = message.text;
+                    await message.answer('enter task priority:');
+                    async def definePriority(message):
+                        context['newTaskPriority'] = message.text;   
+                        context['tasks'].append(Todo(title= context['newTaskTitle'], priority= context['newTaskPriority'])); 
+                    context['steps'].append(definePriority);   
+                context['steps'].append(defineTitle);  
+                return await message.answer('введите загаловок таска:');
+         
+            title, priority = message.text.replace('/create', '').split('-')
+            context['tasks'].append(Todo(title, priority))
+            print(context['tasks']);
+        except ValueError as error:
+             await message.answer('формат вызова команды /create неверный');
 
-    print();
-    print(accountsData);
+
+@dp.message(Command("all"))
+async def command_all_handler(message:Message): 
+    user =  preparation(message);
+    # tasksStr = '\n'.join(map(lambda task:  f"{task.title} - {task.priority}",tasks[message.chat.id]));
+    tasksStr = "all tasks: \n"
+
+    for task in user['tasks']:
+        tasksStr += f'\n{user["tasks"].index(task)+1}. {task.title.strip()}: {task.priority.strip()};'
+
+    await message.answer(tasksStr)
 
 
+class Context:
+    pass
 
+
+users = {};
+def preparation(message):
+    if message.chat.id in users:
+        return users[message.chat.id];
+
+    users[message.chat.id] = {'steps': [], 'tasks': [], 'personal': {'name': '',}}
+    
+    return preparation(message);
+
+newTaskTitle = None;
+newTaskPriority = None;
+@dp.message()
+async def message_handler(message: Message) :
+    context = preparation(message)
+    if len(context['steps']):
+        await context['steps'][0](message);
+        del context['steps'][0]
+    # global newTaskPriority, newTaskTitle;
+    # if isCreateCalled and  newTaskTitle == None:
+    #     newTaskTitle = message.text;
+    #     return await message.answer('enter task priority:');
+    # if isCreateCalled :
+    #     newTaskPriority = message.text;
+    #     accountsData[message.chat.id] = [] if message.chat.id not in  accountsData else accountsData[message.chat.id];
+    #     accountsData[message.chat.id].append(Todo(title=newTaskTitle, priority= newTaskPriority));
+        
+    #     return await message.answer('successfully created');
+    
+    # await command_start_handler(message);
 
 async def main():
     bot = Bot('6935433814:AAHAe5BYlLzNqkvBVcxzcGCFfWtZLKapGKY')
